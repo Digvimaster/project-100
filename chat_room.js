168f798a043215e494d7e31c87a6866dc0143a7d
Digvimaster <89394577+Digvimaster@users.noreply.github.com>
2022-07-15 18:41:00 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCaHftcXo9fx2CevoA8mutp6PE7nwilGd0",
+    authDomain: "lets-chat-1f08d.firebaseapp.com",
+    projectId: "lets-chat-1f08d",
+    storageBucket: "lets-chat-1f08d.appspot.com",
+    messagingSenderId: "1053277523955",
+    appId: "1:1053277523955:web:f924fdefd2ec1fa7270c86"
+  };
+  firebase.initializeApp(firebaseConfig);
